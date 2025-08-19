45e03e6c6cd5061b123762014dec38eb9028ad1e
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-08-19 19:34:54 +0300

    fix

--- project-additions/project-name-by-Ernest/Author.tsx
+++ project-additions/project-name-by-Ernest/Author.tsx
@@ -1,27 +1,31 @@
-type AuthorClass = {
-  authorClass: string;
-  authorTable: string;
-  authorMobile: string;
-};
+import styled from "styled-components";
+
 type AuthorProps = {
-  authorStyles: AuthorClass;
-  isResized: boolean;
   mode: boolean;
   nameAuthor: string
 };
-export default function Author ({ authorStyles, isResized, mode, nameAuthor }: AuthorProps)  {
+
+const AuthorStyles = styled.p<{mode: boolean}>`
+
+transition: all .2s ease;
+  position: absolute; 
+  bottom: 2rem;
+  transition: all .3s ease;
+  user-select: none;
+  left: 2rem;
+  color: ${({mode}) => mode ==  true ? "black": "white"};
+  font-weight: 700; font-family: "monospace";
+  font-size: 2rem;
+`;
+
+export default function Author ({ mode, nameAuthor }: AuthorProps)  {
   return (
+
     <>
-      <p
-        style={{ color: mode == true ? "black" : "white" }}
-        className={`${authorStyles.authorClass} ${
-          isResized == true
-            ? authorStyles.authorTable
-            : authorStyles.authorMobile
-        }`}
-      >
+      <AuthorStyles mode = {mode}>
        {nameAuthor}
-      </p>
+      </AuthorStyles>
     </>
+
   );
 };
