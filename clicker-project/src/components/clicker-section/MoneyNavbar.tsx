a2d5e4057f125712ec2844cb4112b6ed7c035dca
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-08-23 23:21:13 +0300

    Added dynamic money multiplier change

--- clicker-project/src/components/clicker-section/MoneyNavbar.tsx
+++ clicker-project/src/components/clicker-section/MoneyNavbar.tsx
@@ -1,13 +1,16 @@
 type MoneyNavbarProps = {
     clickerCount: number;
-    moneyBalance: number
+    moneyBalance: number;
+    mode: boolean;
+    coefficient: number;
 }
 
-export const MoneyNavbar = ({clickerCount, moneyBalance}: MoneyNavbarProps) => {
+export const MoneyNavbar = ({clickerCount, moneyBalance, mode, coefficient}: MoneyNavbarProps) => {
+
     return (<>
-    <section className="absolute top-10 text-center w-fit h-fit place-self-center">
-        <header className="text-white text-4xl border-solid font-mono border-red-500">money: {moneyBalance}$ <span style = {{color:"gray"}}>(1.0x)</span></header>
-        <p className="text-3xl font-mono text-center">Quantity of clicks: {clickerCount}</p>
+<section className={`${mode === true ? "text-black": "text-white"} absolute top-10 text-center w-fit h-fit place-self-center`}>
+        <header className=" duration-200 text-4xl font-mono">money: {moneyBalance}$ <span style = {{color:"gray"}}>({coefficient}x)</span></header>
+        <p className="text-3xl font-mono duration-200 text-center">Quantity of clicks: {clickerCount}</p>
     </section>
     </>)
 }
