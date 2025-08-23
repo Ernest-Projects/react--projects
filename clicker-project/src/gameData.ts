4c3925a407ef0f410f3f4e1e4a0ce17be3736903
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-08-23 23:03:05 +0300

    add coefficient for upgrader price

--- clicker-project/src/gameData.ts
+++ clicker-project/src/gameData.ts
@@ -4,11 +4,15 @@ export const upgradeDataObject = {
         header: "double click",
         image: "../public/double-click-icon.png",
         id: 0,
+        // multiplier coefficient for increase upgrade price
+        coefficient: 2,
         description: "Gives a chance to double-click! <br> The higher the level, the greater the chance."
     },
     second:  {
         name: "autoclickerLVL",
         id: 1,
+        // multiplier coefficiet for increase upgrade price
+        coefficient: 5,
         image: "../public/autoclicker-icon.png",
         header: "autoclicker",
         description: "Automatically clicks for you. <br> The higher the level, the faster it clicks."
@@ -16,8 +20,9 @@ export const upgradeDataObject = {
     ,third: {
         name: "profitableLVL",
         id: 2,
+        coefficient: 4,
         image: "../public/profitable-icon.png",
-        header: "profitable click",
+        header: "profit click",
         description: "Increases profit from each click. <br> Each level adds +0.1 to earnings."
     },
     //  fourth: {
