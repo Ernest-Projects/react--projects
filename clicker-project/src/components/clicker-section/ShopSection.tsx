52339643c95c6990944c5a26d2a36fdd8d23d5b6
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-08-23 23:19:09 +0300

    A relationship has been established between the parent component and its child component.

--- clicker-project/src/components/clicker-section/ShopSection.tsx
+++ clicker-project/src/components/clicker-section/ShopSection.tsx
@@ -1,7 +1,6 @@
 import { UpgradeClicker } from "../UpgradeClicker";
 import { upgradeDataObject } from "../../gameData";
 
-
 // type upgradeDataObjectProps = {
 //     name: string;
 //     description: string;
@@ -16,6 +15,12 @@ type Upgrader = {
 type ShopSectionProps = {
   isVisible: boolean;
   mode: boolean;
+  bodyWidth: number;
+  moneyBalance: number;
+
+
+  upgradePrices: Upgrader;
+  onUpgradePrices: React.Dispatch<React.SetStateAction<Upgrader>>;
 
   increaseLevels: Upgrader;
   onIncreaseLevels: React.Dispatch<React.SetStateAction<Upgrader>>;
@@ -24,38 +29,50 @@ type ShopSectionProps = {
 export const ShopSection = ({
   isVisible,
   mode,
-  onIncreaseLevels,
-  increaseLevels,
+  onIncreaseLevels, increaseLevels,
+  bodyWidth,
+  moneyBalance,
+  onUpgradePrices, upgradePrices, 
 }: ShopSectionProps) => {
-
-
   return (
     <>
       <section
-        className={`z-10 ${
-          mode === true ? `bg-[rgb(205, 205, 205)] text-black` : `bg-[rgb(20, 20, 20)] text-white`
-        } ${isVisible === true ? "left-[0%]" : "left-[50%]"} duration-200 z-[10] rounded-br-xl rounded-tr-xl w-[50%] transition-[left] h-[96%] absolute my-[1%]`}
+        className={` ${
+          mode === true
+            ? "bg-[rgb(240,240,240)] text-black"
+            : "bg-[rgb(20,20,20)] text-white"
+        } ${
+          (isVisible === true) ? "left-[0%]" : "left-[-60%]"
+        } duration-200 z-[10] rounded-br-xl rounded-tr-xl w-[50%] transition-[left] h-[96%] absolute my-[1%]`}
       >
-        <header className="text-5xl font-mono text-center my-[4rem]">
+        <header className="text-5xl font-mono duration-200 text-center my-[4rem]">
           Upgrage clicker
         </header>
         <div
-          className={`w-full h-[70%] p-[2rem] bg-[rgb(20,20,20)] gap-[2rem] grid grid-col bottom-[1rem]`}
+          className={`w-full h-[70%] p-[2rem] gap-[2rem] grid grid-col bottom-[1rem]`}
         >
           {Object.values(upgradeDataObject).map((upgrader) => {
             return (
               <UpgradeClicker
+              mode = {mode}
+              money = {moneyBalance}
                 level={increaseLevels[upgrader.name]}
 
-                increaseLevel={() =>  onIncreaseLevels((prev) => ({
-                  ...prev,
-                  [upgrader.name]: prev[upgrader.name] + 1,
-                }))
-            }
+                price = {upgradePrices[upgrader.name]}
+                increasePrice = {() => onUpgradePrices((prev) => ({...prev, [upgrader.name]: Number(prev[upgrader.name] * upgrader.coefficient)}))}
+              
+                  
+                increaseLevel={() =>
+                  onIncreaseLevels((prev) => ({
+                    ...prev,
+                    [upgrader.name]: prev[upgrader.name] + 1,
+                  }))
+                }
                 key={upgrader.id}
                 name={upgrader.header}
                 description={upgrader.description}
                 image={upgrader.image}
+                width = {bodyWidth}
               ></UpgradeClicker>
             );
           })}
