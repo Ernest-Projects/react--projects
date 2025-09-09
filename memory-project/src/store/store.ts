9071d8b627f91291c94d0505e91d96dd6446f235
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-09-09 10:40:30 +0300

    store for Redux implementation.

--- memory-project/src/store/store.ts
+++ memory-project/src/store/store.ts
@@ -1,18 +1,29 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// yea its a bit hard to understand. Redux for bigger projects so logic a little harder
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: { value: 0 },
+// yea its a bit hard to understand. Redux for bigger projects so logic may be little harder
+const gameSlice = createSlice({
+  name: "game",
+  initialState: { value: 0, gameStartState: false, animateActive: false, quantityOfCards: 6 },
   reducers: {
+
+    setGameStartState: (state) => {
+      state.gameStartState = !state.gameStartState;
+    },
     increment: (state) => {
       state.value += 1;
     },
+    setAnimateActive: (state, actions) => {
+      state.animateActive = actions.payload;
+    },
+    setQuantityOfCards: (state, actions) => {
+      state.quantityOfCards = actions.payload;
+    }
   },
+
 });
-export const { increment } = counterSlice.actions;
+export const { increment, setGameStartState, setAnimateActive, setQuantityOfCards } = gameSlice.actions;
 export const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  reducer: { game: gameSlice.reducer },
 });
 
 
