dd573b4c5a39ce2854f672f00ed020be6eeba91a
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-09-09 10:37:41 +0300

    cards object

--- memory-project/src/cards.ts
+++ memory-project/src/cards.ts
@@ -20,6 +20,42 @@ export  const Cards = {
       six: {
         id: 5
     },
+       seventh: {
+        id: 6
+    },
+      eight: {
+        id: 7
+    },
+        nine: {
+        id: 8
+    },
+      ten: {
+        id: 9
+    },
+        eleventh: {
+        id: 10
+    },
+      twelwe: {
+        id: 11
+    },
+        thirdteen: {
+        id: 12
+    },
+      fourhteen: {
+        id: 13
+    },
+        fiveteen: {
+        id: 14
+    },
+      sixteen: {
+        id: 15
+    },
+        seventeen: {
+        id: 16
+    },
+      eighteen: {
+        id: 17 
+    },
       
 
 }
