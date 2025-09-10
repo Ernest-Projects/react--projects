305dc944f5a229b4bc75c9adce8b9fd1b2385bcf
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-09-10 21:06:39 +0300

    logic version: 1.1

--- memory-project/src/Memory.tsx
+++ memory-project/src/Memory.tsx
@@ -12,10 +12,10 @@ import { setGameStartState } from "./store/store";
 function Memory() {
   const sleep = (ms: number) => new Promise((res) => setTimeout(res,ms));
   let elements = [];
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < 125; i++) {
     elements.push(
       <div
-        className={` ${styles.backgroundRotate} transform bg-[radial-gradient(rgb(25,25,25),rgb(65,65,65))] transition duration-200 animate-ping overflow-hidden bg-[rgb(45,45,45)] absolute top-[0] aspect-[1/1] w-[100px] relative flex justify-center `}
+        className={` ${styles.backgroundRotate} transform bg-[radial-gradient(rgb(65,65,65),rgb(25,25,25))] transition duration-200 animate-ping overflow-hidden bg-[rgb(45,45,45)] absolute top-[0] aspect-[1/1] w-[100px] relative flex justify-center `}
       >
         {" "}
         <span className=" rounded-[10rem] absolute place-self-center  aspect-[1/1] w-[1rem] z-[5] bg-[rgb(20,20,20)]"></span>{" "}
@@ -40,13 +40,16 @@ function Memory() {
   const dispatch = useAppDispatch();
 
 
-  const gameRef = useRef<HTMLElement>(null);
+  const loadingRef = useRef<HTMLDivElement>(null);
 
   useEffect(()=> {
       const asyncFunction = async () => {
-        const gameStyle = gameRef.current;
-        if (!gameStyle) return
-        await sleep(400);
+        const loadingStyle = loadingRef.current;
+        if (!loadingStyle) return
+        loadingStyle.style.opacity = "1";
+        loadingStyle.style.pointerEvents = "none";
+        await sleep(200);
+        loadingStyle.style.opacity = "0";
        
       }
       asyncFunction()
@@ -56,8 +59,9 @@ function Memory() {
 
   return (
     <>
+    <div ref={loadingRef} className={`bg-[rgb(25,25,25)] transition duration-200 w-[100vw] h-[100vh] pointer-events-none opacity-0 absolute z-[100]`}></div>
       <main
-        ref={gameRef}
+        
         className={` w-[100vw] flex z-[1] justify-center align-center border relative gap-[1%] bg-[rgb(35,35,35)] m-0 p-[1%]  h-[100vh]`}
       > 
         {/* </div> */}
