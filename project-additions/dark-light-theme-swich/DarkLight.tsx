b2fcbc98788d589bca81cb86b12d43fff287a9f2
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-09-11 18:20:11 +0300

    fix

--- project-additions/dark-light-theme-swich/DarkLight.tsx
+++ project-additions/dark-light-theme-swich/DarkLight.tsx
@@ -1,22 +1,22 @@
 import styles from "./DarkLight.module.scss";
 type DarkLightProps = {
-  mode: boolean;
+  
   onSwichTheme: () => void;             
   name: string;
   bodyWidth: number;
   side: boolean;
   widthNumber: number;
 };
-const DarkLight = ({ mode, onSwichTheme, name ,bodyWidth, widthNumber, side}: DarkLightProps) => {
+const DarkLight = ({ onSwichTheme, name ,bodyWidth, widthNumber, side}: DarkLightProps) => {
   return (
     <>
       <section style = {{opacity: bodyWidth > widthNumber ? "1" : "0" }} className={`${styles.darkLight} ${side ? styles.rightSide : styles.leftSide}`}>
         <button
           onClick={() => onSwichTheme()}
-          className={`${mode == true ? styles.lightButton : styles.darkButton}`}
+          className={`${name == "light" ? styles.lightButton : styles.darkButton}`}
         > 
           {" "}
-          <p className={mode == true ? styles.lightClass : styles.darkClass}>
+          <p className={name == "light" ? styles.lightClass : styles.darkClass}>
             {name}
           </p>
         </button>
