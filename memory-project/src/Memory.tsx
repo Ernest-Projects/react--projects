d2ab005948973367d107c91e24a0b41257ef7eed
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-09-15 14:11:51 +0300

    Game is finished! Version of game: 1.2. Good work!

--- memory-project/src/Memory.tsx
+++ memory-project/src/Memory.tsx
@@ -7,12 +7,12 @@ import { SettingWindow } from "./components/SettingsWindow";
 
 import { DarkLight } from "project-additions";
 import { useEffect, useRef } from "react";
-import { setGameStartState } from "./store/store";
+import { setBodyWidth, setGameStartState, setQuantityOfCards, setThemeMode } from "./store/store";
 
 function Memory() {
-  const sleep = (ms: number) => new Promise((res) => setTimeout(res,ms));
+  const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
   let elements = [];
-  for (let i = 0; i < 125; i++) {
+  for (let i = 0; i < 91; i++) {
     elements.push(
       <div
         className={` ${styles.backgroundRotate} transform bg-[radial-gradient(rgb(65,65,65),rgb(25,25,25))] transition duration-200 animate-ping overflow-hidden bg-[rgb(45,45,45)] absolute top-[0] aspect-[1/1] w-[100px] relative flex justify-center `}
@@ -36,55 +36,106 @@ function Memory() {
   }
 
   const isGameState = useAppSelector((state) => state.game.gameStartState);
-  const animate = useAppSelector((state) => state.game.animateActive);
+  const bodyWidth = useAppSelector((state) => state.game.bodyWidth);
+  const quantityOfCards = useAppSelector((state) => state.game.quantityOfCards);
+  const isMode = useAppSelector((state) => state.game.themeMode);
   const dispatch = useAppDispatch();
-
 
   const loadingRef = useRef<HTMLDivElement>(null);
 
-  useEffect(()=> {
-      const asyncFunction = async () => {
-        const loadingStyle = loadingRef.current;
-        if (!loadingStyle) return
-        loadingStyle.style.opacity = "1";
-        loadingStyle.style.pointerEvents = "none";
-        await sleep(200);
-        loadingStyle.style.opacity = "0";
-       
-      }
-      asyncFunction()
-    },[isGameState])
+  const handleStartFinishGame = () => {
+    dispatch(setGameStartState(false))
+    dispatch(setQuantityOfCards(quantityOfCards));
+  }
 
-  // background (STUPID)
+  useEffect(() => {
+    const asyncFunction = async () => {
+      const loadingStyle = loadingRef.current;
+      if (!loadingStyle) return;
+      loadingStyle.style.opacity = "1";
+      loadingStyle.style.pointerEvents = "none";
+      await sleep(200);
+      loadingStyle.style.opacity = "0";
+    };
+    asyncFunction();
+  }, [isGameState]);
+
+  useEffect(() => {
+    const handleChangeThemeKey = (event: KeyboardEvent) => {
+      if (event.ctrlKey && (event.key == "r" || event.key == "R")) {
+        event.preventDefault();
+        dispatch(setThemeMode());
+      }
+       if (event.ctrlKey && (event.key == "e" || event.key == "E")) {
+        event.preventDefault();
+      }
+    }
+    window.addEventListener("keydown", handleChangeThemeKey);
+    return () => (window.removeEventListener("keydown", handleChangeThemeKey))
+  }, )
+
+  useEffect(() => {
+    const resizeBodyWidth = () => {
+      dispatch(setBodyWidth(document.body.offsetWidth));
+    };
+    addEventListener("resize", resizeBodyWidth);
+    return () => removeEventListener("resize", resizeBodyWidth);
+  }, [document.body.offsetWidth]);
 
   return (
     <>
-    <div ref={loadingRef} className={`bg-[rgb(25,25,25)] transition duration-200 w-[100vw] h-[100vh] pointer-events-none opacity-0 absolute z-[100]`}></div>
+      <div
+        ref={loadingRef}
+        className={`bg-[rgb(25,25,25)] transition duration-200 w-[100vw] overflow-[hidden] overflow-hidden h-[100vh] pointer-events-none opacity-0 absolute z-[100]`}
+      ></div>
       <main
-        
-        className={` w-[100vw] flex z-[1] justify-center align-center border relative gap-[1%] bg-[rgb(35,35,35)] m-0 p-[1%]  h-[100vh]`}
-      > 
+        className={` ${isMode ? "invert" : ""} transition duration-200 w-[100vw]  flex z-[1] justify-center align-center border relative gap-[1%] bg-[rgb(35,35,35)] m-0 p-[1%]  h-[100vh]`}
+      >
         {/* </div> */}
-        {isGameState === true ? <button className={`absolute transition duration-200 hover:scale-[1.05] bg-[rgb(20,20,20)] rounded-[.5rem] left-[2rem] font-mono text-white text-2xl top-[2rem] p-4`} onClick= {() => dispatch(setGameStartState())}>Settings</button> : <SettingWindow></SettingWindow>}
+        {isGameState === true ? (
+          <button
+            className={`absolute transition z-[10000] duration-200 hover:scale-[1.05] bg-[rgb(20,20,20)] rounded-[.5rem] left-[2rem] font-mono text-white text-2xl top-[2rem] p-4`}
+            onClick={() => handleStartFinishGame()}
+          >
+            Settings
+          </button>
+        ) : (
+          <SettingWindow></SettingWindow>
+        )}
         <section
           style={{ transform: "translateZ(-5rem)" }}
-          className={`${isGameState === true ? "relative" : "absolute"} perspective-[1500px]  place-self-center right-6  h-[90vh] transition duration-100 `}
-        >
-          <Field></Field>
+          className={`${
+            isGameState === true ? "relative" : "absolute"
+          } perspective-[1500px] place-self-center right-6  h-[90vh] transition duration-100 `}
+        > {(bodyWidth < 1520 && !isGameState) ? "" : <Field></Field>}
         </section>
         <div
           className={`text-4xl text-white font-mono font-bold absolute bottom-[1rem] left-[1rem]`}
         >
           Memory game by Ernest
         </div>
-        {/* background (IT'S WORK) */}
+        {/* background (IT'S WORK!!!) */}
         <div
-          className={`grid absolute  grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-[2rem] z-[-1] grid-rows-[repeat(auto-fill,minmax(100px,1fr))] w-[100vw] top-[0]`}
+          className={` transition duration-200 grid absolute  grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-[2rem] z-[-1] grid-rows-[repeat(auto-fill,minmax(100px,1fr))] w-[100vw] top-[0]`}
         >
           {elements}
         </div>
+          {/* change theme of game */}
+          <div className={`absolute w-[100vw] ${isMode ? "invert" : ""}`}>
+
+        <DarkLight
+          onSwichTheme={() => dispatch(setThemeMode())}
+          bodyWidth={bodyWidth}
+          widthNumber={900}
+          side = {isGameState ? true : false}
+          name={isMode == true ? "light" : "dark"}
+        ></DarkLight>
+          </div>
       </main>
       {/* <DarkLight></DarkLight> */}
     </>
-  )}
-export default Memory
+  );
+}
+export default Memory;
+
+// thanks for your attention!
