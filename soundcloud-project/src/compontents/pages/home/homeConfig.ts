068d6a34fb341ec860f9689ff77b85b603516e8a
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-09-27 01:06:18 +0300

    configuration file with object and others stuff for Home page

---
+++ soundcloud-project/src/compontents/pages/home/homeConfig.ts
@@ -0,0 +1,15 @@
+
+
+
+export const ArtistToolsButtons = [
+    {content: "Amplify", image: "../../../../public/artistTools/amplify.png"},
+    {content: "Replace", image: "../../../../public/artistTools/replace.png"},
+    {content: "Distribute", image: "../../../../public/artistTools/distributive.png"},
+    {content: "Master", image: "../../../../public/artistTools/master.png"},
+    {content: "Monetize", image: "../../../../public/artistTools/monetize.png"},
+    {content: "Spotlight", image: "../../../../public/artistTools/spotlight.png"},
+    {content: "Top fans", image: "../../../../public/artistTools/fans.png"},
+    {content: "Comments ", image: "../../../../public/artistTools/img.png"}
+
+
+]
