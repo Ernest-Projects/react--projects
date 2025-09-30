e572cb514d403e27e7fccca088edd459106790ec
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-10-01 00:50:52 +0300

    inplemented logic for slider moving

--- soundcloud-project/src/redux/storages/homeSlice.ts
+++ soundcloud-project/src/redux/storages/homeSlice.ts
@@ -1,28 +1,57 @@
-import { configureStore, createSlice, type PayloadAction } from '@reduxjs/toolkit';
-import type { InitialEntry } from 'react-router-dom';
+import {
+  configureStore,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
+import type { InitialEntry } from "react-router-dom";
 
-
-type ToolsProps =  {
-    isToolsOpened: boolean;
-}
+type SliderProps = {
+  [key: string]: number;
+};
+type ToolsProps = {
+  isToolsOpened: boolean;
+  sliders: SliderProps;
+};
 
 const initialState = {
-    isToolsOpened: false
+  isToolsOpened: false,
+  sliders: {
+    first: 0,
+    second: 0,
+    third: 0,
+    fourth: 0,
+  },
 } as ToolsProps;
 
 export const homeStore = createSlice({
-    name: "home page",
-    initialState,
-    reducers: {
-        setIsToolsOpened: (state) => {
-            state.isToolsOpened  = !state.isToolsOpened;
-        }
+  name: "home page",
+  initialState,
+  reducers: {
+    //open/close tool button
+    setIsToolsOpened: (state) => {
+      state.isToolsOpened = !state.isToolsOpened;
+    },
+
+    // for sliders
+    setMoveSlider: (
+      state,
+      action: PayloadAction<{ id: string; value: number; direction: string }>
+    ) => {
+      const { id, value, direction } = action.payload;
+      if (direction == "right" && state.sliders[id] <= value * 4) {
+        state.sliders[id] += value;
+      }
+      if (direction == "left" && state.sliders[id] >= 0) {
+        state.sliders[id] -= value;
+      }
     }
-})
+  },
+});
 
-
-export const {setIsToolsOpened} = homeStore.actions;
-export const store = configureStore({reducer: {home_page: homeStore.reducer}})
+export const { setIsToolsOpened, setMoveSlider } = homeStore.actions;
+export const store = configureStore({
+  reducer: { home_page: homeStore.reducer },
+});
 
 export const homeReducer = homeStore.reducer;
 
