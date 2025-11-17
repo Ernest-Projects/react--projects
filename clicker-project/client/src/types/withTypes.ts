a21dfa1de292b1437d372af2f30198a2f6b610c3
popovychErnest <ernestpopovic5621@gmail.com>
2025-11-17 10:16:40 +0200

    typization for Authorization component

---
+++ clicker-project/client/src/types/withTypes.ts
@@ -0,0 +1,30 @@
+// object typization for database
+export type UserDataProps = {
+    type: string,
+    data: Record<string, unknown>
+};
+
+
+// props typization in Authorization compo
+export interface AuthorizationProps {
+  userLogged: boolean;
+  loggedOrRegistered: boolean;
+  
+  onLoggedOrRegistered: () => void;
+  handleSendUserData: (data: UserDataProps) => void;
+};
+
+
+
+// registration
+export type UserRegistrationProps = {
+  user_name: string;
+  user_email: string;
+  user_password: string;
+  confirm_user_password: string;
+};
+// login
+export type UserLoginProps = {
+  user_name: string;
+  user_password: string;
+};
