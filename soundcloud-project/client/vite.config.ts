7357b87bff22945facb8b5292437957e0022f3cb
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-12-24 12:13:30 +0200

    Set base path to '/' in Vite configuration

--- soundcloud-project/client/vite.config.ts
+++ soundcloud-project/client/vite.config.ts
@@ -7,7 +7,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    base: import.meta.env.VITE_BASE_PATH || "/",
+    base: "/",
   server: {
     proxy: {
       // proxy server
