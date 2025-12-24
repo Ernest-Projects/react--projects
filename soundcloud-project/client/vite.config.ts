3ccb3e28de0235591b863170ea98428328c37ab1
Ernest-Projects <ernestpopovic5621@gmail.com>
2025-12-24 11:44:39 +0200

    Set base path for Vite configuration (vercel)

--- soundcloud-project/client/vite.config.ts
+++ soundcloud-project/client/vite.config.ts
@@ -7,6 +7,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    base: process.env.VITE_BASE_PATH || "/react--projects/tree/main/soundcloud-project",
   server: {
     proxy: {
       // proxy server
